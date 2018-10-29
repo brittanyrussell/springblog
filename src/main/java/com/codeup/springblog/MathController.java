9748bd3b68b0212d15c27c6d4a22048776e6ce68
brittanyrussell <brittanyrussell11032gmail.com>
2018-10-29 11:22:40 -0500

    finished controller exercisse

---
+++ src/main/java/com/codeup/springblog/MathController.java
@@ -0,0 +1,34 @@
+package com.codeup.springblog;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MathController {
+    @RequestMapping(path = "/add/{number}/and/{number2}", method = RequestMethod.GET)
+    @ResponseBody
+    public String add(@PathVariable Long number, @PathVariable Long number2) {
+        return "Your total is: " + (number  + number2) + "!";
+    }
+
+    @RequestMapping(path = "/subtract/{number}/from/{number2}", method = RequestMethod.GET)
+    @ResponseBody
+    public String subtract(@PathVariable Long number, @PathVariable Long number2) {
+        return "Your total is: " + (number - number2) + "!";
+    }
+    @RequestMapping(path = "/multiply/{number}/and/{number2}", method = RequestMethod.GET)
+    @ResponseBody
+    public String multiply(@PathVariable Long number, @PathVariable Long number2) {
+        return "Your total is: " + (number * number2) + "!";
+    }
+    @RequestMapping(path = "/divide/{number}/by/{number2}", method = RequestMethod.GET)
+    @ResponseBody
+    public String divide(@PathVariable Long number, @PathVariable Long number2) {
+        return "Your total is: " + (number / number2) + "!";
+    }
+}
+
